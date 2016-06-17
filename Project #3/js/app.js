b3ec087bb07d8a5a9fbc1d1e61745db3a5708855
gracekc <gracekc@users.noreply.github.com>
2016-06-17 11:41:57 -0700

    Update app.js

--- Project #3/js/app.js
+++ Project #3/js/app.js
@@ -1,11 +1,4 @@
 
-echo "# Resume" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/gracekc/Resume.git
-git push -u origin master
-
 
 
 var Enemy = function(x,y,speed) {
